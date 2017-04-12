780d89d9ab4bba64c0cb98918cb919a761a53651
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2017-04-12 17:49:46 +0300

    Update run_sklearn_kmeans to also work for stdin data

--- sequential/run_sklearn_kmeans.py
+++ sequential/run_sklearn_kmeans.py
@@ -3,9 +3,18 @@ import numpy as np
 from sklearn.cluster import KMeans
 from sklearn.cluster import MiniBatchKMeans
 
-with open(sys.argv[1]) as f:
-    n, k, dim= map(int, f.readline().split())
-    X = np.loadtxt(f)
+if(len(sys.argv) == 1):
+	text = sys.stdin.readlines()
+	params = map(int, text[0].split(" "))
+	n, k, dim = params[0], params[1], params[2]
+	x_list = []
+	for line in text[1:]:
+		x_list.append(np.fromstring(line, dtype=float, sep=' '))
+	X = np.array(x_list)
+else:
+	with open(sys.argv[1]) as f:
+	    n, k, dim= map(int, f.readline().split())
+	    X = np.loadtxt(f)
 
 kmeans = KMeans(n_clusters=k, random_state=0, n_jobs=-1, n_init=1).fit(X)
 print "Simple K-Means clustering with K-Means++ initialization:"
