2be9c7503dc258c896e606a81d4caf7de57873d9
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2017-04-12 19:12:00 +0300

    Initial version of compare_implementations script, it executes many implementations of the algorithm for many datasets and keeps all the time and center results in a structure

--- sequential/run_sklearn_kmeans.py
+++ sequential/run_sklearn_kmeans.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import sys
 import numpy as np
 from sklearn.cluster import KMeans
@@ -17,11 +18,11 @@ else:
 	    X = np.loadtxt(f)
 
 kmeans = KMeans(n_clusters=k, random_state=0, n_jobs=-1, n_init=1).fit(X)
-print "Simple K-Means clustering with K-Means++ initialization:"
-print "-Labels:"
-print kmeans.labels_
-print "-Centers:"
-print kmeans.cluster_centers_
+# print "Simple K-Means clustering with K-Means++ initialization:"
+# print "-Labels:"
+# print kmeans.labels_
+# print "-Centers:"
+print '\n'.join(' '.join('%f' %x for x in y) for y in kmeans.cluster_centers_)
 
 # minibatch = MiniBatchKMeans(n_clusters=k, random_state=0, n_init=1).fit(X)
 # print "\nMini-Batch K-Means clustering with K-Means++ initialization:"
